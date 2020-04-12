aeec67fcc29a7f7d4486bc56132c662e7badfb66
JCSR <johnconnorsryan@gmail.com>
2020-04-11 23:54:36 -0400

    timer function integrated with question change.

--- script.js
+++ script.js
@@ -1,50 +1,131 @@
-var count = "1 2 3 4 5";
-var num = count.split(" ");
-var questions = [
-    { q: 'The sky is blue.', a: 't' },
-    { q: 'There are 365 days in a year.', a: 't' },
-    { q: 'There are 42 ounces in a pound.', a: 'f' },
-    { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-    { q: 'Bananas are vegetables.', a: 'f' }
+ 
+  var questions = [
+    { "question": 'What does JS stand for', 
+    "option1": 'Javascript',
+    "option2": 'Just Sauce',
+    "option3": 'Jerry Springer',
+    "option4": 'Justice Served', 
+    "answer": "1" },
+    { "question": 'What version of HTML is commonly used?', 
+    "option1": '3',
+    "option2": '4',
+    "option3": '5',
+    "option4": '12',
+    "answer": "3" },
+    { "question": 'What is used to set something into local storage?', 
+    "option1": '.getItem',
+    "option2": '.setItem',
+    "option3": '.betItem',
+    "option4": '.tetItem',
+    "answer": "2" },
+    { "question": 'What program does the class use to set our Git repos?', 
+    "option1": 'bash',
+    "option2": 'crash',
+    "option3": 'stash',
+    "option4": 'mash',
+    "answer": "1" },
+    { "question": 'How many questions in this quiz so far?', 
+    "option1": '2',
+    "option2": '3',
+    "option3": '5',
+    "option4": 'C',
+    "answer": "3" }
   ];
+  
+  var currentQuestion = 0;
   var score = 0;
+ 
+
+  var container = document.getElementById('quizContainer');
+  var questionEl = document.getElementById('question');
+  var opt1 = document.getElementById('opt1');
+  var opt2 = document.getElementById('opt2');
+  var opt3 = document.getElementById('opt3');
+  var opt4 = document.getElementById('opt4');
+  var nextButton = document.getElementById('nextButton');
+  var resultCont = document.getElementById('result');
+  var totQuestions = questions.length;
 
 
+  var mins = 1;
+  var secs = mins * 60;
 
-var mainEl = document.getElementById("page");
-var timerEl = document.getElementById("timer");
-var countdownEl = document.getElementById("countdown")
-var countFunc = 0;
+  function countDown() {
+      setTimeout('Decrement()', 60);
+  }
 
-var areYouReady = alert("Are you ready to begin?");
+  function Decrement() {
+      if(document.getElementById){
+          minutes = document.getElementById('minutes');
+          seconds = document.getElementById('seconds');
+          if(seconds < 59){
+              seconds.value = secs;
+          } else {
+              minutes.value = getMinutes();
+              seconds.value = getSeconds();
+          } if(mins <1){
+              minutes.style.color = "red";
+              seconds.style.color = "red";
+          } if(mins < 0){
+              alert('Times Up!');
+              minutes.value = 0;
+              seconds.value = 0;
+              container.style.display = 'none';
+              resultCont.style.display = '';
+              resultCont.textContent = "Your score:" + score;
+          } else {
+              secs --;
+              setTimeout('Decrement()', 1000);
+          }
+      }
+  };
 
-function countDownTimer() {
-    var timeleft = 4;
-    var timeInterval = setInterval(function() {
-        countdownEl.textContent = "get ready: " + timeleft;
-        timeleft--;
+  function getMinutes(){
+      mins = Math.floor(secs /60);
+      return mins;
+  };
 
-        if (timeleft === -1) {
-             codeQuiz();
-             scoreFunc();
-            clearInterval(timeInterval);
-            countdownEl.textContent = "";
-        }
-    }, 1000);
-};
+  function getSeconds(){
+      return secs - Math.round(mins * 60);
+  }
 
 
-var scoreFunc = function() {
-    
+  function loadQuestion(questionIndex) {
+      var i = questions[questionIndex];
+      questionEl.textContent = (questionIndex + 1) + '. ' + i.question;
+      opt1.textContent = i.option1;
+      opt2.textContent = i.option2;
+      opt3.textContent = i.option3;
+      opt4.textContent = i.option4;
+  }
+  function loadNextQuestion() {
+      var selectedOption = document.querySelector('input[type=radio]:checked');
+      if(!selectedOption){
+          alert('Choose an answer!');
+          return;
+      }
+      var answer = selectedOption.value;
+      if(questions[currentQuestion].answer == answer){
+          score += 20;
+          secs += 5;
+      }
+      if(questions[currentQuestion].answer != answer){
+        secs -= 25;
+      }
+      selectedOption.checked = false
+      currentQuestion++;
+     
+      
+      if(currentQuestion == totQuestions - 1){
+          nextButton.textContent = 'finished';
+      }
+      if(currentQuestion == totQuestions){
+          container.style.display = 'none';
+          resultCont.style.display = '';
+          resultCont.textContent = "Your score:" + score;
+          return;
+      }
+      loadQuestion(currentQuestion);
+  };
 
-}
-
-
-var codeQuiz = function(){
-for (var i = 0; i < questions.length; i++) {
-
-    }
-};
-
-document.getElementById("countdown").innerHTML = countDownTimer();
-document.getElementById("questions").innerHTML = codeQuiz();
+ loadQuestion(currentQuestion);
